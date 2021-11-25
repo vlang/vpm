85fa07dc293dc2823300cadfe25d883a709fdc48
Anton Zavodchikov <terisbackno@gmail.com>
2021-11-25 09:44:04 +0500

    🧹 Clean up CLI a little bit

--- cmd/cli/main.v
+++ cmd/cli/main.v
@@ -1,111 +1,107 @@
 import os
 import cli
 import toml
-import net.http
+import net.urllib
+// import net.http
 
 const (
-	template_of_main_file =
-'fn main() {
-	println(\'Hello, world!\')
-}
-'
-)
+	template_of_main_file = "module main
 
 fn main() {
-	mut vpm := cli.Command {
+	println('Hello, world!')
+}
+"
+)
+
+fn module_template(project_name string) string {
+	template := '[package]
+name = "$project_name"
+author = "Your Name <example@gmail.com>"
+description = ""
+license = ""
+version = "0.1.0"
+
+[dependencies]
+
+'
+	return template
+}
+
+fn main() {
+	mut vpm := cli.Command{
 		name: 'vpm'
 		description: 'VLang Package Manager'
 		execute: fn (cmd cli.Command) ? {
 			println('Hello from VPM!') // TODO: Create a list of commands in the near future
 		}
 		commands: [
-			cli.Command {
+			cli.Command{
 				name: 'install'
 				execute: fn (cmd cli.Command) ? {
-					json_config := parse_toml_config()
-					println(json_config)
+					_ := parse_toml_config(os.join_path(os.getwd(), 'vpm.toml')) ?
+					// println(json_config)
 				}
-			}
-			cli.Command {
+			},
+			cli.Command{
 				name: 'init'
 				execute: fn (cmd cli.Command) ? {
-					os.mkdir('src') ? {
-						mut main_file := os.create('src/main.v') ?
-						main_file.write_string(template_of_main_file) ?
-
-						create_toml_file(os.base(os.getwd()))
-					}
+					project_path := os.getwd()
+					project_name := os.file_name(project_path)
+					init_project(project_path, project_name) ?
 				}
-			}
-			cli.Command {
+			},
+			cli.Command{
 				name: 'new'
 				execute: fn (cmd cli.Command) ? {
-					project_name := os.args[3]
-					project_path := $if windows {
-						'$os.getwd()\\$project_name\\src'
-					} $else {
-						'$os.getwd()/$project_name/src'
+					if cmd.args.len < 0 {
+						cmd.execute_help()
+						return
 					}
-					
-					os.mkdir_all(project_path) ? {
-						mut main_file := os.create('$project_name/src/main.v') ?
-						main_file.write_string(template_of_main_file) ?
 
-						create_toml_file(project_name)
+					project_name := cmd.args.first()
+					if _ := os.mkdir(project_name) {
+						init_project(os.join_path('.', project_name), project_name) ?
 					}
 				}
-			}
+			},
 		]
 	}
 
 	vpm.setup()
-	vpm.parse(os.args)	
+	vpm.parse(os.args)
 }
 
-fn create_toml_file(project_name string) {
-	init_toml_body :=
-'[package]
-name = "$project_name"
-author = "Your Name <adress@gmail.com>"
-description = ""
-license = ""
-version = "0.0.1"
-
-[dependencies]
-
-'
-
-	mut config := os.create('$project_name/vpm.toml') or {
-		mut config := os.create('vpm.toml') or {
-			panic(err)
-		}
-
-		config.write_string(init_toml_body) or {
-			panic(err)
-		}
-
-		config.close()
-		
-		return
-	}
-
-	config.write_string(init_toml_body) or {
-		panic(err)
-	}
-
-	config.close()
+fn init_project(basepath string, name string) ? {
+	os.write_file(os.join_path(basepath, 'vpm.toml'), module_template(name)) ?
+	os.write_file(os.join_path(basepath, 'main.v'), template_of_main_file) ?
 }
 
-fn parse_toml_config() string {
-	config := toml.parse_file('vpm.toml') or {
-		panic(err)
-	} 
-	
+fn parse_toml_config(filepath string) ?[]string {
+	config := toml.parse_file(filepath) ?
+
+	val := config.value('dependencies')
+
+	if val !is map[string]toml.Any {
+		println('no dependencies')
+		return []string{}
+	}
+
+	dependencies := val.as_map()
+	mut parsed_dependencies := []string{cap: dependencies.len}
+
 	println('>> List of packages:')
-	for package_name, version in config.value('dependencies').as_map() {
-		print ('\t$package_name - $version | ')
+	for package_name, version in dependencies {
+		version_string := version.string()
+		print('\t$package_name - $version_string | ')
+		parsed_dependencies << package_name + '@' + version_string
 
-		http.download_file_with_progress('https://vpm.vlang.io/api/$package_name/$version')
+		escaped_package := urllib.path_escape(package_name)
+		escaped_version := urllib.path_escape(version_string)
+
+		println('Package source: `https://vpm.vlang.io/api/$escaped_package/${escaped_version}.zip`')
+		// http.download_file('https://vpm.vlang.io/api/$escaped_package/$escaped_version.zip',
+		// 	os.join_path('.', parsed_dependencies.last() + '.zip')) ?
 	}
-	println('')
+
+	return parsed_dependencies
 }
