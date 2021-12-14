6142db84385af89a4ab2467f377496acc1336cf4
Anton Zavodchikov <terisbackno@gmail.com>
2021-12-14 17:46:28 +0500

    🧹 Return old packages as is

--- app/old.v
+++ app/old.v
@@ -2,22 +2,14 @@ module app
 
 import json
 import web
-// import github
 
-// Backward compatibility for V <=0.2.2
+// Backward compatibility for V <=0.2.x
 ['/jsmod/:name'; get]
 fn (mut app App) jsmod(name string) web.Result {
-	// repo := github.get_repo_by_id(package.gh_repo_id) or {
-	// 	return wrap_service_error(mut app, err)
-	// }
-
 	old_package := app.services.packages.get_old_package(name) or {
 		return wrap_service_error(mut app, err)
 	}
-	println('`$old_package` cloned...')
+
+	println('`$old_package.name` cloned...')
 	return app.json(.ok, json.encode(old_package))
-	// return app.json(.ok, json.encode(models.OldPackage{
-	// 	...old_package
-	// 	url: repo.clone_url
-	// }))
 }
