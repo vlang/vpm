3e1dfc382973d3e414be8790a5ac82220278b75e
Alexander Medvednikov <alexander@medvednikov.com>
2023-04-17 22:04:59 +0200

    fix /auth

--- auth.v
+++ auth.v
@@ -92,23 +92,24 @@ fn (mut app App) oauth_cb() vweb.Result {
 	return app.redirect('/new')
 }
 
-fn (mut app App) auth() {
+fn (mut app App) auth() vweb.Result {
 	id_cookie := app.get_cookie('id') or {
 		println('failed to id cookie')
-		return
+		return app.redirect('/')
 	}
 	id := id_cookie.int()
 	q_cookie := app.get_cookie('q') or {
 		println('failed to get q cookie.')
-		return
+		return app.redirect('/')
 	}
 	random_id := q_cookie.trim_space()
 	println('auth sid="${id_cookie}" id=${id} len ="${random_id.len}" qq="${random_id}" !!!')
 	app.cur_user = User{}
 	if id != 0 {
-		cur_user := app.retrieve_user(id, random_id) or { return }
+		cur_user := app.retrieve_user(id, random_id) or { return app.redirect('/') }
 		app.cur_user = cur_user
 	}
+	return app.redirect('/')
 }
 
 fn (app &App) login_link() string {
